83800bddec30d9da058d77ba8c1642df9173e957
TROLLred <30495597+TROLLred@users.noreply.github.com>
2023-04-09 20:05:49 +0700

    complete slider

--- src/components/blocks/events-slider/events-slider.ts
+++ src/components/blocks/events-slider/events-slider.ts
@@ -10,24 +10,28 @@ type EventsSliderOptions = ComponentOptions;
 
 export default class EventsSlider extends Component {
     sliderContainer?: HTMLElement;
+    cards?: HTMLElement[];
     slider?: Swiper;
     sliderNavigation?: SliderNavigation;
     constructor(element: ComponentProps, options?: EventsSliderOptions) {
         super(element);
         this.sliderContainer = this.getElement('slider');
         const navigation = getComponent('events-slider__navigation', this.nRoot);
-        if (navigation?.component) {
-            this.sliderNavigation = new SliderNavigation({
-                name: 'slider-navigation',
-                component: navigation.component,
-            });
-        }
-        if (this.sliderContainer) {
-            this.slider = new Swiper(this.nRoot, {
-                spaceBetween: 0,
-                slidesPerView: 1,
-                navigation: this.sliderNavigation?.swiperOptions || false,
-            });
+        this.cards = this.getElements('card');
+        if (this.cards.length > 1) {
+            if (navigation?.component) {
+                this.sliderNavigation = new SliderNavigation({
+                    name: 'slider-navigation',
+                    component: navigation.component,
+                });
+            }
+            if (this.sliderContainer) {
+                this.slider = new Swiper(this.nRoot, {
+                    spaceBetween: 0,
+                    slidesPerView: 1,
+                    navigation: this.sliderNavigation?.swiperOptions || false,
+                });
+            }
         }
     }
 
